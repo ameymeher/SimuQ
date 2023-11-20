f6e35aa0a86be7e53e9d8e2d09ac4f2c42d3d3b9
Amey <meher.amey23@gmail.com>
2023-11-19 23:18:53 -0500

    ZNE code changes

--- thesis/Experiment/Ising Model ZNE execution/ising_zne_experiment.py
+++ thesis/Experiment/Ising Model ZNE execution/ising_zne_experiment.py
@@ -1,21 +1,19 @@
 import time
 import yaml
 import numpy as np
-from hamiltonian_models import Ising
-from evaluation_metrics import TV
-from ibm_provider_custom import IBMProvider
+from SimuQ.thesis.Experiment.utilities.hamiltonian_models import Ising
+from SimuQ.thesis.Experiment.utilities.evaluation_metrics import TV
 from simuq.qutip import QuTiPProvider
 import logging.config
 import logging
 
 #Setup logger
-with open('logging.yaml',encoding="utf8") as ly:
+with open('../../experiments/logging.yaml', encoding="utf8") as ly:
     loggingDict = yaml.safe_load(ly)
 logging.config.dictConfig(loggingDict)
 logger = logging.getLogger("optimization_level_experiment")
 
-
-def experiment(ibm,N,T,system,optimization_level,use_pulse):
+def experiment(ibm,N,T,system,noise_factor):
     
     tv_value = -1
     execution_time = 0
@@ -49,7 +47,7 @@ def experiment(ibm,N,T,system,optimization_level,use_pulse):
         ########################################################################################################################
         #Experiment run
         
-        ibm.compile(Ising_chain, backend=system, trotter_num=4,optimization_level=optimization_level,use_pulse=use_pulse)
+        ibm.compile(Ising_chain, backend=system, trotter_num=4,use_pulse=True, noise_factor=noise_factor)
         ibm.run()
         
         job = ibm.return_job()
@@ -67,42 +65,17 @@ def experiment(ibm,N,T,system,optimization_level,use_pulse):
         #print("Time for the complete execution: {}".format(end_time - start_time))
         
         ########################################################################################################################
-    
+
     finally:
-        return  tv_value, execution_time
+        return tv_value, execution_time
 
 
 
+from ibm_provider_custom import IBMProvider
+ibm = IBMProvider(from_file="../ibm_API_key", hub="ibm-q-ncsu", group="nc-state", project="quantum-compiler")
 
-ibm = IBMProvider(from_file="../ibm_API_key", hub="ibm-q", group="open", project="main")
+for N in range(7,16):
+    for T in range(1,4):
+        for noise_factor in range(6):
+            logger.info(experiment(ibm, N, T, "ibmq_mumbai", noise_factor))
 
-
-# for N in range(3,7):
-#     for T in range(1,4):
-#         for optimization_level in range(1,4):
-#             logger.info(experiment(ibm, N, T, "ibm_nairobi", optimization_level,True))
-#             logger.info(experiment(ibm, N, T, "ibm_nairobi", optimization_level,False))
-            
-#         logger.info(experiment(ibm, N, T, "ibm_nairobi", None,True))
-#         logger.info(experiment(ibm, N, T, "ibm_nairobi", None,False))
-            
-print(experiment(ibm, 4, 1, "ibm_nairobi", 3,True))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 2,True))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 1,True))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", None,True))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 3,0))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 2,1))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 2,0))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 1,1))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 1,0))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 0,1))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", 0,0))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", None,1))
-# print(experiment(ibm, 4, 1, "ibm_nairobi", None,0))
-    
-# for N in range(3,7):
-#         for T in range(1,11):
-#             for optimization_level in range(0,4):
-#                 tv_value , execution_time = experiment(ibm, N, T, "ibm_nairobi", optimization_level))
-
-#             tv_value , execution_time = experiment(ibm, N, T, "ibm_nairobi", None))
